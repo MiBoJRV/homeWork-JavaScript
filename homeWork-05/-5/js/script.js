e980848bd97d68ceb513e18f6935a054b38f8c4a
bodnar.mikhail <bodnar.mikhail@gmail.com>
2019-04-20 18:49:44 +0300

    Переіменовав назви папок-1

---
+++ homeWork-05/-5/js/script.js
@@ -0,0 +1,11 @@
+/*jslint devel: true */
+/*jslint node: true */
+'use strict';
+
+let menu = document.getElementsByClassName('menu'),
+    menuItem = document.getElementsByTagName('li');
+
+
+
+let li = document.createElement('li');
+    li.classList.add('menu-item');
---
+++ homeWork-05/-5/js/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+let menu = document.getElementsByClassName('menu'),
+    menuItem = document.getElementsByTagName('li');
+
+
+
+let li = document.createElement('li');
+    li.classList.add('menu-item');
