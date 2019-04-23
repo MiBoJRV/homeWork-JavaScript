bea2a39cee935bfd7b4c784fc21d2a5d1829cb75
bodnar.mikhail <bodnar.mikhail@gmail.com>
2019-04-23 14:54:39 +0300

    Виконав homeWork-5

--- homeWork-05/-5/js/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-
-let menu = document.getElementsByClassName('menu'),
-    menuItem = document.getElementsByTagName('li');
-
-
-
-let li = document.createElement('li');
-    li.classList.add('menu-item');
