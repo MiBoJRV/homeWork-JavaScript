6644bb9c228b4c38d0a57efa887d29f302dd3968
bodnar.mikhail <bodnar.mikhail@gmail.com>
2019-04-26 14:28:03 +0300

    Виконав Виконав homeWork-12

---
+++ homeWork-11/js/homeWork-11.js
@@ -0,0 +1,40 @@
+
+// доп задание homeWork-11 ---------------------------------------------------------------------------------------------
+// <input id="age" value="30">
+// let age = document.getElementById('age');
+// function showUser(surname, name) {
+// 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// };
+// showUser();
+
+
+// Выведите на экран правильное сообщение, которое берет значение из input
+//showUser.apply(age, ["Дед","Мороз"]);  // метод преподователя
+//или
+//showUser.call(age, 'Дед', 'Мороз');
+
+
+//homeWork-11
+// class Options {
+// 	constructor(height, width, bg, fontSize, textAlign) {
+// 		this.height = height;
+// 		this.width = width;
+// 		this.bg = bg;
+// 		this.fontSize = fontSize;
+// 		this.textAlign = textAlign;
+// 	}
+
+// 	createDiv() {
+// 		let elem = document.createElement('div');
+// 		document.body.appendChild(elem);
+// 		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+// 		elem.style.cssText = param;
+// 	}
+// }
+
+// const item = new Options(300, 350, "red", 14, "center");
+
+// item.createDiv();
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
